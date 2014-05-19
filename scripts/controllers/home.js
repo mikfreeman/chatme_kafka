eca2dab4824a24f61969dea82d5578e82e17d951
Michael Freeman <mikfreeman@gmail.com>
2014-05-19 23:15:10 +0100

    Added Rooms

--- scripts/controllers/home.js
+++ scripts/controllers/home.js
@@ -4,4 +4,15 @@ angular.module('chatme.controllers')
   .controller('HomeController', 
     ['$scope',
       function($scope) {
+      	$scope.rooms = [{
+      		'roomId' : 'programming',
+      		'label' : 'Programming'
+
+      	},{
+      		'roomId' : 'humour',
+      		'label' : 'Humour'
+      	},{
+      		'roomId' : 'photography',
+      		'label' : 'Photography' 
+      	}]
   }]);
