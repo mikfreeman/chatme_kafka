6510a57e795094d458eb20e6f163dd71925ac7eb
Michael Freeman <mikfreeman@gmail.com>
2015-08-13 07:39:35 +0100

    Adding flash on temp nickname

--- public/scripts/controllers/nickName.js
+++ public/scripts/controllers/nickName.js
@@ -13,6 +13,7 @@ angular.module('chatme.controllers')
 
             chatService.on('connected', function (message) {
                 $scope.nickName = message.nickName;
+                $scope.flash = 'flash';
             });
 
         }]);
